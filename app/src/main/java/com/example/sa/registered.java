1b9aaeeb966f0e24f9cdae2ab4161b739b889750
bangbangbean <jack0001515@gmail.com>
2019-05-11 17:15:19 +0800

    new activity

--- app/src/main/java/com/example/sa/registered.java
+++ app/src/main/java/com/example/sa/registered.java
@@ -2,12 +2,19 @@ package com.example.sa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class registered extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_registered);
+
+        //設定隱藏標題
+        //getSupportActionBar().hide();
+        //設定隱藏狀態
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
