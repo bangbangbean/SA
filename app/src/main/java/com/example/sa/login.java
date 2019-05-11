d9ff84af73f740b34f91c14c123db0b1dafdec4a
406401238 <cj6ej04ul64282561@gmail.com>
2019-05-11 22:03:31 +0800

    ccc

--- app/src/main/java/com/example/sa/login.java
+++ app/src/main/java/com/example/sa/login.java
@@ -2,12 +2,27 @@ package com.example.sa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+
 
 public class login extends AppCompatActivity {
 
+    private TextView tv4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tv4 = (TextView) findViewById(R.id.textView4);
+        tv4.setOnClickListener(new OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(login.this, registered.class);
+                startActivity(intent);
+            }
+        });
     }
 }
