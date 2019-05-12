cdd4e32098936f9d59abeaea716e1651b58e14bc
bangbangbean <jack0001515@gmail.com>
2019-05-12 15:53:39 +0800

    sliderview

---
+++ app/src/main/java/com/example/sa/SliderViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.sa;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class SliderViewPagerAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private  Integer [] images = {R.drawable.a1,R.drawable.a2,R.drawable.a3};
+    public SliderViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.sliderviewpage,null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view,0);
+        return view;
+    }
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+    }
+}
--- app/src/main/java/com/example/sa/startpage.java
+++ app/src/main/java/com/example/sa/startpage.java
@@ -1,53 +1,93 @@
 package com.example.sa;
 
-import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class startpage extends AppCompatActivity {
-    private ImageView image1;
-    //定义一个int数组来存放图片，在R文件中图片是以int形式存在的
-    int[] images = new int[] { R.drawable.a1,
-            R.drawable.a2,
-            R.drawable.a3,
-            };
-    //定义一图片计数器
-    int currentImg = 0;
-    //定义一个handler来进行隔时间操作
-    private Handler handler = new Handler();
-    //新建一个runnable对象
-    private Runnable myRunnable = new Runnable() {
-        public void run() {
-            //每隔1秒切换一次
-            handler.postDelayed(this, 1000);
 
-            if (currentImg >= 2) {
-                currentImg = -1;
-            }
-            // 改变ImageView里显示的图片
-            image1.setImageResource(images[++currentImg]);
-
-        }
-    };
+    ViewPager viewPager;
+    SliderViewPagerAdapter adapter;
+    LinearLayout sliderDots;
+    private int dotCounts;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startpage);
-        image1 = (ImageView) findViewById(R.id.imageView);
-        //初始化时显示第一张图片
-        image1.setImageResource(images[0]);
-        Button btn = (Button) findViewById(R.id.registerbutton);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        viewPager = findViewById(R.id.viewPager);
+        adapter = new SliderViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+
+        sliderDots = findViewById(R.id.SliderDots);
+
+
+        dotCounts = adapter.getCount();
+        dots = new ImageView[dotCounts];
+
+        for(int i=0;i<dotCounts;i++) {
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.slideshow_nonactive_dot));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 2, 8, 0);
+
+            sliderDots.addView(dots[i], params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.slideshow_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
 
             @Override
-            public void onClick(View v) {
-                //将请求提交给runnable
-                handler.post(myRunnable);
+            public void onPageSelected(int position) {
+
+                for(int i=0;i<dotCounts;i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.slideshow_nonactive_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.slideshow_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
 
             }
         });
-    }}
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(),3000,6000);
+
+
+    }
+    private class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            startpage.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(viewPager.getCurrentItem()==0){
+                        viewPager.setCurrentItem(1);
+                    }else if(viewPager.getCurrentItem()==1){
+                        viewPager.setCurrentItem(2);
+                    }else{
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
+    }
+}
