676de6b17c4b9ff4879b39216c499332f6ea44d6
406401238 <cj6ej04ul64282561@gmail.com>
2019-05-12 15:17:19 +0800

    ccc

--- app/src/main/java/com/example/sa/login.java
+++ app/src/main/java/com/example/sa/login.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.content.Intent;
 import android.view.View.OnClickListener;
 
+import org.w3c.dom.Text;
+
 
 public class login extends AppCompatActivity {
 
     private TextView tv4;
+    private TextView tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,15 @@ public class login extends AppCompatActivity {
             public void onClick(View v)
             {
                 Intent intent= new Intent(login.this, registered.class);
+                startActivity(intent);
+            }
+        });
+        tv3 = (TextView) findViewById(R.id.textView3);
+        tv3.setOnClickListener(new OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(login.this, forget_password.class);
                 startActivity(intent);
             }
         });
