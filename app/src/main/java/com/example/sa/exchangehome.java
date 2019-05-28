edea34873f61fa344165dc458fc8e44a3419d5cc
ColinChiu87 <colinchiu87@gmail.com>
2019-05-28 11:38:02 +0800

    edit exchange page

--- app/src/main/java/com/example/sa/exchangehome.java
+++ app/src/main/java/com/example/sa/exchangehome.java
@@ -1,13 +1,41 @@
 package com.example.sa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class exchangehome extends AppCompatActivity {
+
+    private Button shopbt;
+    private Button exchangebt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchangehome);
+
+        shopbt = (Button) findViewById(R.id.functionlist_shopbt);
+        shopbt.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(exchangehome.this, startpage.class);
+                startActivity(intent);
+            }
+        });
+
+        exchangebt = (Button) findViewById(R.id.functionlistexchangebt);
+        exchangebt.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(exchangehome.this, exchangehome.class);
+                startActivity(intent);
+            }
+        });
     }
 }
