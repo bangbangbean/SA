416513190cee3a7529aaaf7e2db5de261fb0342a
bangbangbean <jack0001515@gmail.com>
2019-06-05 10:59:15 +0800

    OK

--- app/src/main/java/com/example/sa/test1.java
+++ app/src/main/java/com/example/sa/test1.java
@@ -12,13 +12,13 @@ public interface test1<一般商品> {
     // POST網址     https://jsonplaceholder.typicode.com/albums
     // ...typicode.com/[這裡就是API的路徑]
 
-    @GET("normolgood?api_key=keyq0EkjYQRAzfrUN")    // 設置一個GET連線，路徑為albums/1
+    @GET("normalgood?api_key=keyq0EkjYQRAzfrUN")    // 設置一個GET連線，路徑為albums/1
     Call<shopcar> getShopcar();    // 取得的回傳資料用Albums物件接收，連線名稱取為getAlbums
 
-    @GET("normolgoo?api_key=keyq0EkjYQRAzfrUN")    // 設置一個GET連線，路徑為albums/1
+    @GET("normalgood?api_key=keyq0EkjYQRAzfrUN")    // 設置一個GET連線，路徑為albums/1
     Call<checkout1> getCheck1();    // 取得的回傳資料用Albums物件接收，連線名稱取為getAlbums
 
-    @GET("normolgoo/{id}") // 用{}表示路徑參數，@Path會將參數帶入至該位置
+    @GET("normalgood/{id}") // 用{}表示路徑參數，@Path會將參數帶入至該位置
     Call<Req> getShopcarById(@Path("id") String id);
 
     @POST("albums") // 用@Body表示要傳送Body資料
